406407775500d984340a6ce6d1da8ed5a085475f
Yuchen Cao <yuchencao0126@gmail.com>
2017-02-12 16:13:17 -0500

    Add Welcome Activity

---
+++ app/src/main/java/com/example/m4/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.m4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button btnGotoLogin = (Button)findViewById(R.id.btn_goto_login);
+        Button btnGotoRegistration = (Button)findViewById(R.id.btn_goto_registration);
+        btnGotoLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
