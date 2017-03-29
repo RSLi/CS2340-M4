af880a5552f695212788733d980e69f08b05b34e
RSLi <robertsli@163.com>
2017-03-29 17:46:34 -0400

    Add Javadoc in WaterPurityReport an WaterSourceReport

--- app/src/main/java/com/example/m4/models/WaterPurityReport.java
+++ app/src/main/java/com/example/m4/models/WaterPurityReport.java
@@ -11,26 +11,44 @@ public class WaterPurityReport extends Report
         return waterOverallCondition;
     }
 
+    /**
+     * Set the WaterOverallCondition of the report to the desired condition object
+     * @param waterOverallCondition the condition of the report to be submitted
+     */
     public void setWaterOverallCondition(WaterOverallCondition waterOverallCondition)
     {
         this.waterOverallCondition = waterOverallCondition;
     }
 
+    /**
+     * @return the Virus density in ppm
+     */
     public Double getVirusPPM()
     {
         return virusPPM;
     }
 
+    /**
+     * @param virusPPM The virus ppm of the report
+     */
     public void setVirusPPM(Double virusPPM)
     {
         this.virusPPM = virusPPM;
     }
 
+    /**
+     *
+     * @return Contaminant index in ppm
+     */
     public Double getContaminantPPM()
     {
         return contaminantPPM;
     }
 
+    /**
+     *
+     * @param contaminantPPM The contaminant value in ppm that wants be be recorded
+     */
     public void setContaminantPPM(Double contaminantPPM)
     {
         this.contaminantPPM = contaminantPPM;
--- app/src/main/java/com/example/m4/models/WaterSourceReport.java
+++ app/src/main/java/com/example/m4/models/WaterSourceReport.java
@@ -5,21 +5,33 @@ public class WaterSourceReport extends Report
     private WaterCondition waterCondition;
     private WaterType waterType;
 
+    /**
+     * @return The WaterCondition object representing the waterCondition of the reported site
+     */
     public WaterCondition getWaterCondition()
     {
         return waterCondition;
     }
 
+    /**
+     * @param waterCondition The waterCondition that needs to be recorded in the report
+     */
     public void setWaterCondition(WaterCondition waterCondition)
     {
         this.waterCondition = waterCondition;
     }
 
+    /**
+     * @return The water type reported
+     */
     public WaterType getWaterType()
     {
         return waterType;
     }
 
+    /**
+     * @param waterType The water type that needs to be reported
+     */
     public void setWaterType(WaterType waterType)
     {
         this.waterType = waterType;
